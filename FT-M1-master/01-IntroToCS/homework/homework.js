5b20fc199595f41d1e5e315c1e6f192df30cfd2a
GAveroArias <guillermo.avero19@gmail.com>
2023-11-07 11:48:37 -0300

    homework 1 GAA FT-45B

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,10 +1,23 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+    var numeroDecimal = parseInt(num, 2);
+    return numeroDecimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+    if (num === 0) {
+        return "0";
+    }
+    var binario = "";
+    while (num > 0) {
+        binario = (num % 2) + binario;
+        num = ~~(num / 2);
+    }
+    return binario;
+}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+    BinarioADecimal,
+    DecimalABinario,
 };
