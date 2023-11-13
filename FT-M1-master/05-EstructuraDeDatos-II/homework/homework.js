e674e717a010b8450ad0bf630bd3770046011d45
GAveroArias <guillermo.avero19@gmail.com>
2023-11-13 14:26:46 -0300

    Homework EstructuraDeDatos2

--- FT-M1-master/05-EstructuraDeDatos-II/homework/homework.js
+++ FT-M1-master/05-EstructuraDeDatos-II/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /* EJERCICIO 1
 Implementar la clase LinkedList, definiendo los siguientes métodos:
@@ -10,9 +10,82 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
-function LinkedList() {}
 
-function Node(value) {}
+function Node(value) {
+    this.value = value;
+    this.next = null;
+}
+
+function LinkedList() {
+    this.head = null;
+    this._length = 0;
+}
+
+LinkedList.prototype.add = function (value) {
+    let node = new Node(value);
+    if (this.head === null) {
+        this.head = node;
+    } else {
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+    }
+    this._length++;
+    return node;
+};
+
+LinkedList.prototype.remove = function () {
+    let current = this.head;
+    if (this._length === 0) return null;
+    else if (this._length === 1) {
+        let aux = current.value;
+        this.head = null;
+        this._length--;
+        return aux;
+    }
+    while (current.next.next) {
+        current = current.next;
+    }
+    let aux = current.next.value;
+    current.next = null;
+    this._length--;
+    return aux;
+};
+
+LinkedList.prototype.search = function (target) {
+    let current = this.head;
+
+    while (current !== null) {
+        if (typeof target === "function") {
+            if (target(current.value)) {
+                return current.value;
+            }
+        } else if (typeof target === "string") {
+            if (current.value === target) {
+                return current.value;
+            }
+        } else if (current.value === target) {
+            return current.value;
+        }
+
+        current = current.next;
+    }
+    return null;
+};
+
+const linkedList = new LinkedList();
+linkedList.add(1);
+console.log(linkedList.remove());
+console.log(linkedList);
+linkedList.add("two");
+linkedList.add(2);
+console.log(linkedList.search(2));
+console.log(linkedList.search("two"));
+console.log(linkedList);
+console.log(linkedList.remove());
+console.log(linkedList);
 
 /* EJERCICIO 2
 Implementar la clase HashTable.
@@ -27,13 +100,57 @@ La clase debe tener los siguientes métodos:
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() {}
+function HashTable() {
+    this.numBuckets = 35;
+    this.buckets = [];
+}
+
+HashTable.prototype.hash = function (key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+        let code = key.charCodeAt(i);
+        total = total + code;
+    }
+    let index = total % this.numBuckets;
+    return index;
+};
+
+HashTable.prototype.set = function (key, value) {
+    if (typeof key !== "string") {
+        throw new TypeError("Keys must be strings");
+    }
+    let index = this.hash(key);
+    if (!this.buckets[index]) {
+        this.buckets[index] = {};
+    }
+    this.buckets[index][key] = value;
+};
+
+HashTable.prototype.get = function (key) {
+    let index = this.hash(key);
+
+    return this.buckets[index][key];
+};
+
+HashTable.prototype.hasKey = function (key) {
+    let index = this.hash(key);
+    return (
+        this.buckets[index] !== undefined &&
+        this.buckets[index][key].hasOwnProperty(key)
+    );
+};
+
+// const myHashTable = new HashTable();
+// console.log(myHashTable);
+// console.log(myHashTable.hash("Hola"));
+// console.log(myHashTable.hash("chao"));
+// console.log(myHashTable.set("instructora", "Ani"));
+// console.log(myHashTable.get("instructora"));
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
-   Node,
-   LinkedList,
-   HashTable,
+    Node,
+    LinkedList,
 };
